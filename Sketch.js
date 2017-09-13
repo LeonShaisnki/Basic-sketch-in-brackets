e0dd061d05098bbbff29fc3bf04e29e91b590a38
LeonShaisnki <31663389+LeonShaisnki@users.noreply.github.com>
2017-09-13 11:41:42 -0400

    Creating sketch.js

---
+++ Sketch.js
@@ -0,0 +1,27 @@
+var counter = 0;
+var value = 0;
+
+function setup() {
+    createCanvas (1000, 1000);
+}
+function draw() {
+    background (255, 176, 80);
+    fill (value);
+    ellipse(mouseX, mouseY, 20, 20);
+    
+    
+    
+
+}
+        function mouseClicked() 
+    {
+        
+        if (value==0)
+            {
+                value=255;
+            }
+        else 
+        {
+            value = 0;
+        }
+    }
